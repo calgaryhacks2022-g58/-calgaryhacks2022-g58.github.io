25aff721a3840cd6c774decfee403acb6e5562c4
Nathaniel <nathaniel.habtegerge@ucalgary.ca>
2022-02-19 13:29:59 -0700

    added a accountability partner

---
+++ src/layouts/dashboard/components/AccountabilityPartner/index.js
@@ -0,0 +1,28 @@
+import * as React from "react";
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import Typography from "@mui/material/Typography";
+import { CardActionArea } from "@mui/material";
+
+// Images
+import bgImage from "assets/images/bg-sign-in-basic.jpeg";
+
+export default function AccountabilityPartner() {
+  return (
+    <Card>
+      <CardActionArea>
+        <CardMedia component="img" height="140" image={bgImage} alt="Partner Picture" />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="div">
+            Albert Karlo
+          </Typography>
+          <Typography variant="body2" color="text.secondary">
+            Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging
+            across all continents except Antarctica
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+    </Card>
+  );
+}
--- src/layouts/dashboard/index.js
+++ src/layouts/dashboard/index.js
@@ -25,7 +25,6 @@ import DashboardNavbar from "examples/Navbars/DashboardNavbar";
 import Footer from "examples/Footer";
 import ReportsBarChart from "examples/Charts/BarCharts/ReportsBarChart";
 import ReportsLineChart from "examples/Charts/LineCharts/ReportsLineChart";
-import ComplexStatisticsCard from "examples/Cards/StatisticsCards/ComplexStatisticsCard";
 
 // Data
 import reportsBarChartData from "layouts/dashboard/data/reportsBarChartData";
@@ -34,6 +33,10 @@ import reportsLineChartData from "layouts/dashboard/data/reportsLineChartData";
 // Dashboard components
 import Projects from "layouts/dashboard/components/Projects";
 import OrdersOverview from "layouts/dashboard/components/OrdersOverview";
+import AccountabilityPartner from "layouts/dashboard/components/AccountabilityPartner";
+
+// Images
+// import bgImage from "assets/images/bg-sign-in-basic.jpeg";
 
 function Dashboard() {
   const { sales, tasks } = reportsLineChartData;
@@ -42,66 +45,10 @@ function Dashboard() {
     <DashboardLayout>
       <DashboardNavbar />
       <MDBox py={3}>
-        <Grid container spacing={3}>
-          <Grid item xs={12} md={6} lg={3}>
-            <MDBox mb={1.5}>
-              <ComplexStatisticsCard
-                color="dark"
-                icon="weekend"
-                title="Bookings"
-                count={281}
-                percentage={{
-                  color: "success",
-                  amount: "+55%",
-                  label: "than lask week",
-                }}
-              />
-            </MDBox>
-          </Grid>
-          <Grid item xs={12} md={6} lg={3}>
-            <MDBox mb={1.5}>
-              <ComplexStatisticsCard
-                icon="leaderboard"
-                title="Today's Users"
-                count="2,300"
-                percentage={{
-                  color: "success",
-                  amount: "+3%",
-                  label: "than last month",
-                }}
-              />
-            </MDBox>
-          </Grid>
-          <Grid item xs={12} md={6} lg={3}>
-            <MDBox mb={1.5}>
-              <ComplexStatisticsCard
-                color="success"
-                icon="store"
-                title="Revenue"
-                count="34k"
-                percentage={{
-                  color: "success",
-                  amount: "+1%",
-                  label: "than yesterday",
-                }}
-              />
-            </MDBox>
-          </Grid>
-          <Grid item xs={12} md={6} lg={3}>
-            <MDBox mb={1.5}>
-              <ComplexStatisticsCard
-                color="primary"
-                icon="person_add"
-                title="Followers"
-                count="+91"
-                percentage={{
-                  color: "success",
-                  amount: "",
-                  label: "Just updated",
-                }}
-              />
-            </MDBox>
-          </Grid>
+        <Grid item xs={12} md={12} lg={6}>
+          <MDBox mb={1.5}>
+            <AccountabilityPartner />
+          </MDBox>
         </Grid>
         <MDBox mt={4.5}>
           <Grid container spacing={3}>
